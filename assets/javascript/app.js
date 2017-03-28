c2065b5395a5f9430f04cac4909a94dc5d0e177c
Erin Stewart <dragonkittyrawr@users.noreply.github.com>
2017-03-28 17:03:27 -0500

    Initial Commit.  Includes link to firebase.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCXHIOSXpBAo4XGw-md_dy5ibUgjztl8po",
+    authDomain: "trainscheduler-ec804.firebaseapp.com",
+    databaseURL: "https://trainscheduler-ec804.firebaseio.com",
+    storageBucket: "trainscheduler-ec804.appspot.com",
+    messagingSenderId: "673061351665"
+  };
+  firebase.initializeApp(config);
+
